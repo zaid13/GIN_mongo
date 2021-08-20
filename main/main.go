0d75c060089defd9b6e46007154fae182e2eacf2
zaid13 <xaidsaeed@gmail.com>
2021-08-20 18:47:05 +0500

    port number changed

--- main/main.go
+++ main/main.go
@@ -67,5 +67,5 @@ func main() {
 	r.GET("/tasks/", handleGetTasks)
 	r.PUT("/tasks/", handleCreateTask)
 	r.POST("/tasks/", handleUpdateTask)
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(":8090") // listen and serve on 0.0.0.0:8080
 }
